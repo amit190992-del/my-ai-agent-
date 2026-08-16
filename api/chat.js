69e2ea1ab2ee2649f84616b1dd2ea5e6030bc348
amit190992-del <amit190992@gmail.com>
2026-08-16 19:41:53 +0530

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -29,8 +29,7 @@ export default async function handler(req, res) {
     }
 
     return res.status(200).json({
-      reply: data.output_text
-    });
+      reply: data.output?.[0]?.content?.[0]?.text || "AI se reply nahi mila"
 
   } catch (error) {
     return res.status(500).json({
